8fb4587b431c9874e1215295371230b2f7a498c3
Dmytro Bilokha <dmytro@posteo.net>
2017-12-25 21:27:48 +0200

    Added jacoco plugin and trivial test

---
+++ nibee-ejb/src/test/java/com/dmytrobilokha/nibee/ejb/config/ConfigServiceTest.java
@@ -0,0 +1,12 @@
+package com.dmytrobilokha.nibee.ejb.config;
+
+import org.junit.Test;
+
+public class ConfigServiceTest {
+
+    @Test
+    public void checkItAlwaysWorks() {
+        return;
+    }
+
+}
