acc539dd4a9df353af5fd4dc1da191217aec792a
Dmytro Bilokha <dmytro@posteo.net>
2018-02-18 17:58:25 +0200

    Created a separate module for integration tests

---
+++ nibee-it/src/test/java/com/dmytrobilokha/nibee/ejb/post/PostServiceIT.java
@@ -0,0 +1,11 @@
+package com.dmytrobilokha.nibee.ejb.post;
+
+import org.junit.jupiter.api.Test;
+
+public class PostServiceIT {
+
+    @Test
+    public void dumbTest() {
+        System.out.println("Hello, world from the test");
+    }
+}
