75cb06ef460794186ca8fd0b0fb8b1081ffc1ab8
Dmytro Bilokha <dmytro@posteo.net>
2018-02-22 21:19:10 +0200

    Made tag immutable

--- nibee-data/src/main/java/com/dmytrobilokha/nibee/data/Tag.java
+++ nibee-data/src/main/java/com/dmytrobilokha/nibee/data/Tag.java
@@ -2,22 +2,20 @@ package com.dmytrobilokha.nibee.data;
 
 public class Tag {
 
-    private Long id;
-    private String name;
+    private final Long id;
+    private final String name;
+
+    public Tag(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
 
     public Long getId() {
         return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
     }
 
     public String getName() {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
 }
