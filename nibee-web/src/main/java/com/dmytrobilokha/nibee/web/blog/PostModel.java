ee8e1a6795f314fc26c97b36424f0caa87031af6
Dmytro Bilokha <dmytro@posteo.net>
2018-02-25 15:15:50 +0200

    Dropped redirect in favor of using base html tag

--- nibee-web/src/main/java/com/dmytrobilokha/nibee/web/blog/PostModel.java
+++ nibee-web/src/main/java/com/dmytrobilokha/nibee/web/blog/PostModel.java
@@ -8,15 +8,21 @@ import java.util.Collection;
 public class PostModel extends AbstractModel {
 
     private final String entryFileUrl;
+    private final String contentBase;
     private final Collection<Tag> tags;
 
-    PostModel(String entryFileUrl, Collection<Tag> tags) {
+    PostModel(String entryFileUrl, String contentBase, Collection<Tag> tags) {
         this.entryFileUrl = entryFileUrl;
+        this.contentBase = contentBase;
         this.tags = tags;
     }
 
     public String getEntryFileUrl() {
         return entryFileUrl;
+    }
+
+    public String getContentBase() {
+        return contentBase;
     }
 
     public Collection<Tag> getTags() {
