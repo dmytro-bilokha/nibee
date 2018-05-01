789f3d78aa918c92dda00cb5bc3b34a442fe64ec
Dmytro Bilokha <dmytro@posteo.net>
2018-05-01 19:56:04 +0300

    Started implementing logging requests

---
+++ nibee-data/src/main/java/com/dmytrobilokha/nibee/data/WebRequestLogEntry.java
@@ -0,0 +1,203 @@
+package com.dmytrobilokha.nibee.data;
+
+import java.time.LocalDateTime;
+
+public class WebRequestLogEntry {
+
+    private Long id;
+    private String uuid;
+    private String sessionId;
+    private int serverPort;
+    private String requestUri;
+    private String queryString;
+    private String referer;
+    private String userAgent;
+    private String acceptEncoding;
+    private LocalDateTime timeStamp;
+    private int clientPort;
+    private String clientIp;
+
+    WebRequestLogEntry() {
+        //The constructor required by MyBatis
+    }
+
+    private WebRequestLogEntry(Builder builder) {
+        this.uuid = builder.uuid;
+        this.sessionId = builder.sessionId;
+        this.serverPort = builder.serverPort;
+        this.requestUri = builder.requestUri;
+        this.queryString = builder.queryString;
+        this.referer = builder.referer;
+        this.userAgent = builder.userAgent;
+        this.acceptEncoding = builder.acceptEncoding;
+        this.clientPort = builder.clientPort;
+        this.clientIp = builder.clientIp;
+    }
+
+    public static Builder getBuilder() {
+        return new Builder();
+    }
+
+    @Override
+    public String toString() {
+        return "WebRequestLogEntry{" +
+                "id=" + id +
+                ", uuid='" + uuid + '\'' +
+                ", sessionId='" + sessionId + '\'' +
+                ", serverPort=" + serverPort +
+                ", requestUri='" + requestUri + '\'' +
+                ", queryString='" + queryString + '\'' +
+                ", referer='" + referer + '\'' +
+                ", userAgent='" + userAgent + '\'' +
+                ", acceptEncoding='" + acceptEncoding + '\'' +
+                ", timeStamp=" + timeStamp +
+                ", clientPort=" + clientPort +
+                ", clientIp='" + clientIp + '\'' +
+                '}';
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getUuid() {
+        return uuid;
+    }
+
+    public String getSessionId() {
+        return sessionId;
+    }
+
+    public int getServerPort() {
+        return serverPort;
+    }
+
+    public String getRequestUri() {
+        return requestUri;
+    }
+
+    public String getQueryString() {
+        return queryString;
+    }
+
+    public String getReferer() {
+        return referer;
+    }
+
+    public String getUserAgent() {
+        return userAgent;
+    }
+
+    public String getAcceptEncoding() {
+        return acceptEncoding;
+    }
+
+    public LocalDateTime getTimeStamp() {
+        return timeStamp;
+    }
+
+    public int getClientPort() {
+        return clientPort;
+    }
+
+    public String getClientIp() {
+        return clientIp;
+    }
+
+    public static class Builder {
+
+        private String uuid;
+        private String sessionId;
+        private Integer serverPort;
+        private String requestUri;
+        private String queryString;
+        private String referer;
+        private String userAgent;
+        private String acceptEncoding;
+        private Integer clientPort;
+        private String clientIp;
+
+        private Builder() {
+            //Not going to instantiate builder outside
+        }
+
+        public WebRequestLogEntry build() {
+            validate();
+            return new WebRequestLogEntry(this);
+        }
+
+        private void validate() {
+            boolean invalid = uuid == null || sessionId == null || serverPort == null || requestUri == null
+                    || clientPort == null || clientIp == null;
+            if (invalid) {
+                throw new IllegalStateException("Unable to create WebRequestLogEntry from invalid " + this);
+            }
+        }
+
+        @Override
+        public String toString() {
+            return "Builder{" +
+                    "uuid='" + uuid + '\'' +
+                    ", sessionId='" + sessionId + '\'' +
+                    ", serverPort=" + serverPort +
+                    ", requestUri='" + requestUri + '\'' +
+                    ", queryString='" + queryString + '\'' +
+                    ", referer='" + referer + '\'' +
+                    ", userAgent='" + userAgent + '\'' +
+                    ", acceptEncoding='" + acceptEncoding + '\'' +
+                    ", clientPort=" + clientPort +
+                    ", clientIp='" + clientIp + '\'' +
+                    '}';
+        }
+
+        public Builder uuid(String uuid) {
+            this.uuid = uuid;
+            return this;
+        }
+
+        public Builder sessionId(String sessionId) {
+            this.sessionId = sessionId;
+            return this;
+        }
+
+        public Builder serverPort(int port) {
+            this.serverPort = port;
+            return this;
+        }
+
+        public Builder requestUri(String requestUri) {
+            this.requestUri = requestUri;
+            return this;
+        }
+
+        public Builder queryString(String queryString) {
+            this.queryString = queryString;
+            return this;
+        }
+
+        public Builder referer(String referer) {
+            this.referer = referer;
+            return this;
+        }
+
+        public Builder userAgent(String userAgent) {
+            this.userAgent = userAgent;
+            return this;
+        }
+
+        public Builder acceptEncoding(String acceptEncoding) {
+            this.acceptEncoding = acceptEncoding;
+            return this;
+        }
+
+        public Builder clientPort(int port) {
+            this.clientPort = port;
+            return this;
+        }
+
+        public Builder clientIp(String ip) {
+            this.clientIp = ip;
+            return this;
+        }
+    }
+}
---
+++ nibee-web/src/main/java/com/dmytrobilokha/nibee/web/requestlog/LoggingFilter.java
@@ -0,0 +1,86 @@
+package com.dmytrobilokha.nibee.web.requestlog;
+
+import com.dmytrobilokha.nibee.data.WebRequestLogEntry;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Optional;
+import java.util.UUID;
+
+@WebFilter(urlPatterns = "/*")
+public class LoggingFilter implements Filter {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(LoggingFilter.class);
+    private static final String UUID_PARAM_NAME = "uuid";
+    private static final String USER_AGENT = "User-Agent";
+    private static final String REFERER = "Referer";
+    private static final String ACCEPT_ENCODING = "Accept-Encoding";
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        LOGGER.info("LoggingFilter initialized");
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+        HttpServletRequest req = (HttpServletRequest) request;
+        HttpServletResponse resp = (HttpServletResponse) response;
+        HttpSession session = req.getSession();
+        String sessionUuid = (String) session.getAttribute(UUID_PARAM_NAME);
+        if (sessionUuid == null) {
+            sessionUuid = getOrCreateCookieUuid(req, resp);
+            session.setAttribute(UUID_PARAM_NAME, sessionUuid);
+        }
+        WebRequestLogEntry logEntry = WebRequestLogEntry.getBuilder()
+                .sessionId(session.getId())
+                .uuid(sessionUuid)
+                .requestUri(req.getRequestURI())
+                .queryString(req.getQueryString())
+                .serverPort(req.getServerPort())
+                .clientIp(req.getRemoteAddr())
+                .clientPort(req.getRemotePort())
+                .userAgent(req.getHeader(USER_AGENT))
+                .acceptEncoding(req.getHeader(ACCEPT_ENCODING))
+                .referer(req.getHeader(REFERER))
+                .build();
+        LOGGER.info("Got request {}", logEntry);
+        chain.doFilter(request, response);
+    }
+
+    private String getOrCreateCookieUuid(HttpServletRequest req, HttpServletResponse resp) {
+        Cookie[] cookies = req.getCookies();
+        if (cookies != null) {
+            Optional<Cookie> cookieOptional = Arrays.stream(cookies)
+                    .filter(cookie -> UUID_PARAM_NAME.equals(cookie.getName()))
+                    .findFirst();
+            if (cookieOptional.isPresent()) {
+                return cookieOptional.get().getValue();
+            }
+        }
+        String uuid = UUID.randomUUID().toString();
+        Cookie uuidCookie = new Cookie(UUID_PARAM_NAME, uuid);
+        uuidCookie.setMaxAge(100_000_000);
+        resp.addCookie(uuidCookie);
+        return uuid;
+    }
+
+    @Override
+    public void destroy() {
+        LOGGER.info("LoggingFilter destroyed");
+    }
+
+}
