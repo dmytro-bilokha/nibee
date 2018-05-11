9a0c385d745f80b788653ba7556c545d38fa5391
Dmytro Bilokha <dmytro@posteo.net>
2018-05-11 21:24:07 +0300

    Added showing build properties on the page template

--- nibee-web/src/main/java/com/dmytrobilokha/nibee/web/initialization/Initializator.java
+++ nibee-web/src/main/java/com/dmytrobilokha/nibee/web/initialization/Initializator.java
@@ -1,8 +1,13 @@
 package com.dmytrobilokha.nibee.web.initialization;
 
+import com.dmytrobilokha.nibee.service.config.ConfigProperty;
+import com.dmytrobilokha.nibee.service.config.ConfigService;
 import com.dmytrobilokha.nibee.service.initialization.InitializationService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.inject.Inject;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
@@ -10,21 +15,29 @@ import javax.servlet.annotation.WebListener;
 @WebListener
 public class Initializator implements ServletContextListener {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(Initializator.class);
+
     private InitializationService initializationService;
+    private ConfigService configService;
 
     @Inject
-    public Initializator(InitializationService initializationService) {
+    public Initializator(InitializationService initializationService, ConfigService configService) {
         this.initializationService = initializationService;
+        this.configService = configService;
     }
 
     @Override
     public void contextInitialized(ServletContextEvent sce) {
         initializationService.onAppStart();
+        ServletContext context = sce.getServletContext();
+        context.setAttribute("appVersion", configService.getAsString(ConfigProperty.APP_VERSION));
+        context.setAttribute("buildTimestamp", configService.getAsString(ConfigProperty.BUILD_TIMESTAMP));
+        LOGGER.info("Web application initialization completed");
     }
 
     @Override
     public void contextDestroyed(ServletContextEvent sce) {
-        //Do nothing for now...
+        LOGGER.info("Web application destroyed");
     }
 
 }
