36dc93238a4b2b8752d6dd75266e93f6f93bb58b
Dmytro Bilokha <dmytro@posteo.net>
2018-10-28 13:44:07 +0200

    Added Bulma css framework

--- admin-frontend/src/main.js
+++ admin-frontend/src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faCoffee } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import 'bulma/css/bulma.css';
 import App from './App.vue';
 import router from './router';
 import store from './store';
--- admin-frontend/src/views/About.vue
+++ admin-frontend/src/views/About.vue
@@ -2,5 +2,13 @@
   <div class="about">
     <h1>This is an about page</h1>
     <font-awesome-icon icon="coffee" />
+    <div class="columns">
+      <div class="column is-1 is-pulled-left">
+        Here is a one-fifth column example
+      </div>
+      <div class="column">
+        This column should be wider than previous one.
+      </div>
+    </div>
   </div>
 </template>
