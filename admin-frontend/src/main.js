64cc98500adbf35bb22a6e9d4ea8b9efcbc2376c
Dmytro Bilokha <dmytro@posteo.net>
2018-10-28 17:17:37 +0200

    Added custom style asset to override Bulma defaults

--- admin-frontend/src/main.js
+++ admin-frontend/src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faCoffee } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
-import 'bulma/css/bulma.css';
+import './assets/styles.scss';
 import App from './App.vue';
 import router from './router';
 import store from './store';
