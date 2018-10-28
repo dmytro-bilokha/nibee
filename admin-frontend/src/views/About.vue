2671e7c5a82709269d684f0b223ba4ae2aadc3d4
Dmytro Bilokha <dmytro@posteo.net>
2018-10-28 19:36:24 +0200

    Postponed bulma customizing

--- admin-frontend/src/views/About.vue
+++ admin-frontend/src/views/About.vue
@@ -1,10 +1,18 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
-    <font-awesome-icon icon="coffee" />
     <div class="columns">
       <div class="column is-1 is-pulled-left">
-        Here is a one-fifth column example
+        <aside class="menu">
+          <p class="menu-label">
+            Post
+          </p>
+          <ul class="menu-list">
+            <li><a>Create</a></li>
+            <li><a>Delete</a></li>
+            <li><a>Modify <font-awesome-icon icon="coffee" /></a></li>
+          </ul>
+        </aside>
       </div>
       <div class="column">
         This column should be wider than previous one.
