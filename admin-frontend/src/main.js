aa699f17a5704d69137572b07bfac5232d3bc740
Dmytro Bilokha <dmytro@posteo.net>
2018-10-28 13:11:28 +0200

    Added fontawesome Vue component

--- admin-frontend/src/main.js
+++ admin-frontend/src/main.js
@@ -1,10 +1,14 @@
 import Vue from 'vue';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import App from './App.vue';
 import router from './router';
 import store from './store';
 
+library.add(faCoffee);
+Vue.component('font-awesome-icon', FontAwesomeIcon);
 Vue.config.productionTip = false;
-
 new Vue({
   router,
   store,
