1d22d0e130b54812a3e8f405d151dbd09c564050
Dmytro Bilokha <dmytro@posteo.net>
2018-11-16 20:22:03 +0200

    Introduced Buefy components set

--- admin-frontend/src/main.js
+++ admin-frontend/src/main.js
@@ -1,14 +1,14 @@
 import Vue from 'vue';
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { faCoffee } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import Buefy from 'buefy';
+import 'buefy/dist/buefy.css';
 import './assets/styles.scss';
 import App from './App.vue';
 import router from './router';
 import store from './store';
 
-library.add(faCoffee);
-Vue.component('font-awesome-icon', FontAwesomeIcon);
+Vue.use(Buefy, {
+  defaultIconPack: 'fas'
+});
 Vue.config.productionTip = false;
 new Vue({
   router,
--- admin-frontend/src/views/About.vue
+++ admin-frontend/src/views/About.vue
@@ -2,7 +2,7 @@
   <div class="about">
     <h1>This is an about page</h1>
     <div class="columns">
-      <div class="column is-1 is-pulled-left">
+      <div class="column is-1 is-pulled-left right-line">
         <aside class="menu">
           <p class="menu-label">
             Post
@@ -10,13 +10,19 @@
           <ul class="menu-list">
             <li><a>Create</a></li>
             <li><a>Delete</a></li>
-            <li><a>Modify <font-awesome-icon icon="coffee" /></a></li>
+            <li><a>Modify</a></li>
           </ul>
         </aside>
       </div>
       <div class="column">
-        This column should be wider than previous one.
-      </div>
+        This column should be wider than previous one.<i class="fas fa-archive"></i> Icon?
+        <b-field label="Select a date">
+          <b-datepicker
+            placeholder="Click to select..."
+            icon="calendar">
+          </b-datepicker>
+        </b-field>
+      </div>        
     </div>
   </div>
 </template>
