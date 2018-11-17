4e69681698b27a5ceb92606b8cf073e738f0c544
Dmytro Bilokha <dmytro@posteo.net>
2018-11-17 16:27:26 +0200

    Started implementing admin servlet to upload the new post archive

---
+++ backend/nibee-web/src/main/java/com/dmytrobilokha/nibee/web/admin/managepost/PostUploadServlet.java
@@ -0,0 +1,47 @@
+package com.dmytrobilokha.nibee.web.admin.managepost;
+
+import com.dmytrobilokha.nibee.service.config.ConfigProperty;
+import com.dmytrobilokha.nibee.service.config.ConfigService;
+
+import javax.inject.Inject;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
+import java.io.File;
+import java.io.IOException;
+
+@WebServlet("/admin/post-upload")
+@MultipartConfig(
+        fileSizeThreshold = 1024 * 1024 * 1
+        , maxFileSize = 1024 * 1024 * 50
+        , maxRequestSize = 1024 * 1024 * 100
+        , location = "/"
+)
+public class PostUploadServlet extends HttpServlet {
+
+    private static final String UPLOAD_DIR = "_UPLOADS_";
+
+    private final ConfigService configService;
+
+    @Inject
+    public PostUploadServlet(ConfigService configService) {
+        this.configService = configService;
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String blogBaseDir = configService.getAsString(ConfigProperty.POSTS_ROOT);
+        String uploadPath = blogBaseDir + File.separator + UPLOAD_DIR;
+        for (Part part : req.getParts()) {
+            String fileName = part.getSubmittedFileName();
+            part.write(uploadPath + File.separator + fileName);
+        }
+        resp.setStatus(HttpServletResponse.SC_OK);
+        resp.getWriter().println("OK");
+    }
+
+}
