9efb6a92222d652e5f327ec17de60b440a58df0b
Dmytro Bilokha <dmytro@posteo.net>
2018-11-17 22:05:40 +0200

    Implemented new post creation form UI draft

--- admin-frontend/src/views/About.vue
+++ admin-frontend/src/views/About.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
     <div class="columns">
       <div class="column is-1 is-pulled-left right-line">
         <aside class="menu">
@@ -15,14 +14,46 @@
         </aside>
       </div>
       <div class="column">
-        This column should be wider than previous one.<i class="fas fa-archive"></i> Icon?
-        <b-field label="Select a date">
-          <b-datepicker
-            placeholder="Click to select..."
-            icon="calendar">
-          </b-datepicker>
-        </b-field>
+        <section class="section">
+        <div class="container has-text-left">
+          <b-field horizontal label="Post file" class="file">
+            <b-upload v-model="file">
+              <a class="button is-primary" v-if="!file">
+                <b-icon icon="upload"></b-icon>
+                <span>Click to choose</span>
+              </a>
+              <a class="button is-primary" v-if="file">
+                <b-icon icon="edit"></b-icon>
+                <span>{{ file.name }}</span>
+              </a>
+            </b-upload>
+          </b-field>
+          <b-field horizontal label="Select a date">
+            <b-datepicker
+              placeholder="Click to select..."
+              icon="calendar">
+            </b-datepicker>
+          </b-field>
+          <b-field horizontal label="Subject" type="is-danger" message="Please enter a subject">
+            <b-input name="subject" expanded></b-input>
+          </b-field>
+          <b-field horizontal label="From">
+            <b-input name="name" placeholder="Name" expanded></b-input>
+            <b-input name="email" type="email" placeholder="nobody@nowhere.com" expanded></b-input>
+          </b-field>
+        </div>
+        </section>
       </div>        
     </div>
   </div>
 </template>
+
+<script>
+    export default {
+        data() {
+            return {
+                file: null
+            }
+        }
+    }
+</script>
