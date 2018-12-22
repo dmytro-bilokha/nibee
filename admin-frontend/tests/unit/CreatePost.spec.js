09c1f0daf017961576a08cf664e1102cbbba7fdb
Dmytro Bilokha <dmytro@posteo.net>
2018-12-22 19:29:58 +0200

    Added webpack rewire plugin for mocking, added more tests

--- admin-frontend/tests/unit/CreatePost.spec.js
+++ admin-frontend/tests/unit/CreatePost.spec.js
@@ -1,4 +1,3 @@
-
 import { expect } from 'chai';
 import sinon from 'sinon';
 import { mount
@@ -29,13 +28,38 @@ const findCheckboxByName = (wrap, checkboxName) => {
     .filter(componentWrapper => componentWrapper.props().name === checkboxName).at(0);
 };
 
+const findErrorMessage = wrap => {
+  return wrap.find('p.help.is-danger');
+};
+
+const controlNames = { file: 'file'
+                     , title: 'title'
+                     , webPath: 'webPath'
+                     , fsPath: 'fsPath'
+                     , shareable: 'shareable'
+                     , commentAllowed: 'commentAllowed'
+                     , tags: 'tags'
+                     , clear: 'clear'
+                     , submit: 'submit'
+                     };
+
 describe('CreatePost.vue', () => {
   let store;
   let postActions;
   let createPostActions;
+  let mockValidator;
   
   beforeEach(() => {
     const mockPostStore = mockStoreModuleActions(postStore);
+    mockValidator = { quickCheckField: sinon.stub()
+                    , quickCheckFields: sinon.stub()
+                    , fullCheckField: sinon.stub()
+                    , fullCheckFields: sinon.stub()
+                    };
+    CreatePost.__Rewire__('quickCheckField', mockValidator.quickCheckField);
+    CreatePost.__Rewire__('quickCheckFields', mockValidator.quickCheckFields);
+    CreatePost.__Rewire__('fullCheckField', mockValidator.fullCheckField);
+    CreatePost.__Rewire__('fullCheckFields', mockValidator.fullCheckFields);
     postActions = mockPostStore.actions;
     const mockCreatePostStore = mockStoreModuleActions(createPostStore);
     createPostActions = mockCreatePostStore.actions;
@@ -82,60 +106,126 @@ describe('CreatePost.vue', () => {
     expect(store.getters[`createPost/${boxName}`]).to.equal(!valueBefore);
   };
   
+  const checkButtonClearsInput = inputName => {
+    const wrapper = mount(CreatePost, { store, localVue });
+    const input = wrapper.find(`input[name="${inputName}"]`);
+    const newFieldValue = 'this-value-fits-all-inputs';
+    input.setValue(newFieldValue);
+    expect(input.element.value).to.not.be.empty;
+    wrapper.find(`button[name="${controlNames.clear}"]`).trigger('click');
+    expect(input.element.value).to.be.empty;
+  };
+  
+  const checkValidatorCalledOnInputChange = inputName => {
+    const wrapper = mount(CreatePost, { store, localVue });
+    const input = wrapper.find(`input[name="${inputName}"]`);
+    const newFieldValue = 'this-value-fits-all-inputs';
+    input.setValue(newFieldValue);
+    sinon.assert.calledOnce(mockValidator.quickCheckField);
+    sinon.assert.calledWith(mockValidator.quickCheckField, inputName, newFieldValue);
+  };
+  
+  const checkShowsError = inputName => {
+    const errorMessage = `${inputName}-validation-failed`;
+    mockValidator.quickCheckFields.returns({ [inputName]: errorMessage });
+    const wrapper = mount(CreatePost, { store, localVue });
+    expect(findErrorMessage(wrapper).html()).to.include(errorMessage);
+  };
+  
   it('triggers fetching available tags', () => {
     const wrapper = shallowMount(CreatePost, { store, localVue });
     sinon.assert.calledOnce(postActions.fetchAvailableTags);
   });
   
   it('propagates title from store to the input field', () => {
-    checkStoreToInputFlow('title', 'new_Title');
+    checkStoreToInputFlow(controlNames.title, 'new_Title');
   });
   
   it('propagates title input change to the store', () => {
-    checkInputToStoreFlow('title', 'TheTitle');
+    checkInputToStoreFlow(controlNames.title, 'TheTitle');
   });
   
   it('propagates web path from store to the input field', () => {
-    checkStoreToInputFlow('webPath', 'new-web-path');
+    checkStoreToInputFlow(controlNames.webPath, 'new-web-path');
   });
   
   it('propagates web path input change to the store', () => {
-    checkInputToStoreFlow('webPath', 'the-web-path-as-is');
+    checkInputToStoreFlow(controlNames.webPath, 'the-web-path-as-is');
   });
   
   it('propagates fs path from store to the input field', () => {
-    checkStoreToInputFlow('fsPath', 'new-fs-path');
+    checkStoreToInputFlow(controlNames.fsPath, 'new-fs-path');
   });
   
   it('propagates fs path input change to the store', () => {
-    checkInputToStoreFlow('fsPath', 'the-fs-path-as-is');
+    checkInputToStoreFlow(controlNames.fsPath, 'the-fs-path-as-is');
   });
   
   it('propagates commentAllowed from store to the input field', () => {
-    checkStoreToBoxFlow('commentAllowed');
+    checkStoreToBoxFlow(controlNames.commentAllowed);
   });
   
   it('propagates commentAllowed input change to the store', () => {
-    checkBoxToStoreFlow('commentAllowed');
+    checkBoxToStoreFlow(controlNames.commentAllowed);
   });
   
   it('propagates shareable from store to the input field', () => {
-    checkStoreToBoxFlow('shareable');
+    checkStoreToBoxFlow(controlNames.shareable);
   });
   
   it('propagates shareable input change to the store', () => {
-    checkBoxToStoreFlow('shareable');
+    checkBoxToStoreFlow(controlNames.shareable);
   });
     
-  it('clears title on button click', () => {
-    const wrapper = mount(CreatePost, { store, localVue });
-    const titleInput = wrapper.find('input[name="title"]');
-    titleInput.setValue('The new blog post title');
-    expect(titleInput.element.value).to.not.be.empty;
-    wrapper.find('button[name="clear"]').trigger('click');
-    expect(titleInput.element.value).to.be.empty;
+  it('clears title on clear button clicked', () => {
+    checkButtonClearsInput(controlNames.title);
   });
   
-  //TODO: add checks validator is called and error message shown
+  it('clears web path on clear button clicked', () => {
+    checkButtonClearsInput(controlNames.webPath);
+  });
+  
+  it('clears fs path on clear button clicked', () => {
+    checkButtonClearsInput(controlNames.fsPath);
+  });
+  
+  it('calls quick check fields on form opened', () => {
+    const wrapper = shallowMount(CreatePost, { store, localVue });
+    sinon.assert.calledOnce(mockValidator.quickCheckFields);
+  });
+    
+  it('calls quick check on title change', () => {
+    checkValidatorCalledOnInputChange(controlNames.title);
+  });
+  
+  it('calls quick check on web path change', () => {
+    checkValidatorCalledOnInputChange(controlNames.webPath);
+  });
+  
+  it('calls quick check on fs path change', () => {
+    checkValidatorCalledOnInputChange(controlNames.fsPath);
+  });
+  
+  it('calls full check on submit clicked', () => {
+    const wrapper = mount(CreatePost, { store, localVue });
+    wrapper.find(`button[name="${controlNames.submit}"]`).trigger('click');
+    sinon.assert.calledOnce(mockValidator.fullCheckFields);
+  });
+  
+  it('shows validation error for title field', () => {
+    checkShowsError(controlNames.title);
+  });
+  
+  it('shows validation error for web path field', () => {
+    checkShowsError(controlNames.webPath);
+  });
+
+  it('shows validation error for fs path field', () => {
+    checkShowsError(controlNames.fsPath);
+  });
+  
+  it('shows validation error for file field', () => {
+    checkShowsError(controlNames.file);
+  });
     
 });
